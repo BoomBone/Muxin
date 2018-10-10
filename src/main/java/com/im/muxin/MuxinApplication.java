05da7b0e38f4dcb0ed28d129b355134643188563
Ting <ticccccc@163.com>
2018-10-10 15:14:41 +0800

    first commit

---
+++ src/main/java/com/im/muxin/MuxinApplication.java
@@ -0,0 +1,12 @@
+package com.im.muxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuxinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuxinApplication.class, args);
+    }
+}
