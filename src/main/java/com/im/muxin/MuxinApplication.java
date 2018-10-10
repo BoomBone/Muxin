7d33d417d04c71e81e2ea00ba5f315cd4c3cb529
Ting <ticccccc@163.com>
2018-10-10 16:04:25 +0800

    mybatis配置

--- src/main/java/com/im/muxin/MuxinApplication.java
+++ src/main/java/com/im/muxin/MuxinApplication.java
@@ -2,8 +2,12 @@ package com.im.muxin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.im.muxin.mapper")
+@ComponentScan(basePackages = {"com.im"})
 public class MuxinApplication {
 
     public static void main(String[] args) {
